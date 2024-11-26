d3954d22f9f81d24d5bc37c359f045de76c30bed
malcatraz <32576278+malcatraz@users.noreply.github.com>
2024-11-26 13:19:39 -0500

    Add files via upload

---
+++ Chrome.ps1
@@ -0,0 +1,12 @@
+﻿ /silent /install
+# Define the path to the installer
+$installerPath = ".\Chromesetup.exe"
+
+# Define the arguments for silent installation
+$arguments = "/silent /install"
+
+# Start the process and wait for it to complete
+Start-Process $installerPath -ArgumentList $arguments -Wait -NoNewWindow
+
+$logFile = "C:\Path\To\LogFile.log"
+$arguments += " /L*V `"$logFile`""
