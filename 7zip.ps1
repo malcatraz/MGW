0ed26ee68d149fc4b557ac491f409a523a911225
malcatraz <32576278+malcatraz@users.noreply.github.com>
2024-11-26 13:03:26 -0500

    Add files via upload

---
+++ 7zip.ps1
@@ -0,0 +1,11 @@
+﻿# Define the path to the installer
+$installerPath = ".\7z2408-x64.exe"
+
+# Define the arguments for silent installation
+$arguments = "/S"
+
+# Start the process and wait for it to complete
+Start-Process $installerPath -ArgumentList $arguments -Wait -NoNewWindow
+
+$logFile = "C:\Path\To\LogFile.log"
+$arguments += " /L*V `"$logFile`""
