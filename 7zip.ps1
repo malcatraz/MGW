da1bf44e347b89b841b472b6badfa8ceffc64f18
malcatraz <32576278+malcatraz@users.noreply.github.com>
2024-12-02 09:01:12 -0500

    Update 7zip.ps1

--- 7zip.ps1
+++ 7zip.ps1
@@ -7,5 +7,5 @@ $arguments = "/S"
 # Start the process and wait for it to complete
 Start-Process $installerPath -ArgumentList $arguments -Wait -NoNewWindow
 
-$logFile = "C:\Path\To\LogFile.log"
+$logFile = "C:\LogFile.log"
 $arguments += " /L*V `"$logFile`""
